92b0523a1829b09f4986a51758ac07a21c0031d5
David Burt <burt.david.j@gmail.com>
2025-09-19 22:29:38 +1000

    push to main

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 
 import { DataProvider } from "./context/DataContext";
-import { supabase } from "../lib/supabase"; 
+import { supabase } from "../lib/supabase";
 import { getSpotifyData } from "@/lib/spotify";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
@@ -9,10 +9,8 @@ import Footer from "@/components/Footer";
 
 async function fetchData() {
   try {
-  const { data: albums, error } = await supabase.from("Albums").select("*").order('id', { ascending: true });;
-  const spotify = await getSpotifyData();
-   console.log('spotify data in layout.tsx', spotify);
-   console.log('supabase data:', albums[80]);
+    const { data: albums, error } = await supabase.from("Albums").select("*").order('id', { ascending: true });;
+    const spotify = await getSpotifyData();
     return { albums, spotify };
 
   } catch (error) {
@@ -23,14 +21,22 @@ async function fetchData() {
 export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  
-  const initialData = await fetchData(); 
+
+  const initialData = await fetchData();
 
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Ultra&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <NavBar />
-        <DataProvider initialData={ initialData }>{children}</DataProvider>
+        <DataProvider initialData={initialData}>{children}</DataProvider>
         <Footer />
       </body>
     </html>
